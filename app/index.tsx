ca59760c9d514598d871cdbf87c100aeabc222df
Aditya Patwardhan <adityap.thecool@gmail.com>
2024-10-28 19:22:05 -0500

    Updated index

--- app/index.tsx
+++ app/index.tsx
@@ -10,48 +10,15 @@ import LoginScreen from './pages/LoginScreen';
 const Stack = createStackNavigator();
 
 export default function Index() {
-    return (
-        <NavigationContainer  independent={true}>
-          <Stack.Navigator initialRouteName="Land">
-            <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen name="Map" component={MapScreen} />
-            <Stack.Screen name="Land" component={LandingScreen} />
-            <Stack.Screen name="Login" component={LoginScreen} />
-            {/*<Stack.Screen name="Sign" component={SignScreen} />*/}
-          </Stack.Navigator>
-        </NavigationContainer>
-      );
+  return (
+    <NavigationContainer  independent={true}>
+      <Stack.Navigator initialRouteName="Land">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Map" component={MapScreen} />
+        <Stack.Screen name="Land" component={LandingScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        {/*<Stack.Screen name="Sign" component={SignScreen} />*/}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
-
-// import { AppRegistry } from 'react-native';
-// import App from './App';
-// import appConfig from '../app.json'; // Default import for app.json
-
-// // Use appConfig.expo.name to access the name
-// AppRegistry.registerComponent(appConfig.expo.name, () => App);
-
-// import { AppRegistry } from 'react-native';
-// import App from './App';
-// import appConfig from '../app.json'; // Default import for app.json
-// import { Unmatched } from 'expo-router';
-// export default Unmatched;
-
-// console.log("inside index.tsx");
-// // Use appConfig.expo.name to access the name
-// AppRegistry.registerComponent(appConfig.expo.name, () => App);
-// import React from "react";
-// import { Text, View } from "react-native";
-
-// export default function Index() {
-//   return (
-//     <View
-//       style={{
-//         flex: 1,
-//         justifyContent: "center",
-//         alignItems: "center",
-//       }}
-//     >
-//       <Text>Edit app/index.tsx to edit this screen. This should be edited</Text>
-//     </View>
-//   );
-// }
